8f44721ea988388b821d25a1a9175f9b8803ce0f
jedar.girish <jedar.girish@hotmail.com>
2025-10-07 23:45:24 +0530

    Day 1: Added OOP refresher examples (Encapsulation, Abstraction, Inheritance, Polymorphism)

---
+++ OOPsExercises/OOPsDemo/Inheritance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsDemo
+{
+    // OOP/InheritanceExample.cs
+
+    public class Vehicle
+    {
+        public string Brand { get; set; }
+        public void Start() => Console.WriteLine($"{Brand} started.");
+    }
+
+    public class Car : Vehicle
+    {
+        public int Doors { get; set; }
+        public void Honk() => Console.WriteLine($"{Brand} is honking!");
+    }
+
+    class Inheritance
+    {
+        static void Main()
+        {
+            var car = new Car { Brand = "Tesla", Doors = 4 };
+            car.Start();
+            car.Honk();
+        }
+    }
+
+}
